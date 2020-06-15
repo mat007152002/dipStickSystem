b814efdbc85ae71700d58ed3b51487615ff5b282
Matthew Ling <mat007152002g@gmail.com>
2020-06-15 10:36:31 +0800

    Initial Commit

---
+++ dipStickSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dipStickSystem
+//
+//  Created by 旌榮 凌 on 2020/6/15.
+//  Copyright © 2020 旌榮 凌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
